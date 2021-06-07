e62c8503617e38fffe91ff9d681d3dccd7070c82
Fed0d <loldffgf@yandex.ru>
2021-06-07 22:57:37 +0300

    SQL Connect

--- src/com/simple/dimple/bot/App.java
+++ src/com/simple/dimple/bot/App.java
@@ -5,9 +5,35 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.io.IOException;
 import java.util.Properties;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App {
+    private static final String url = "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11417745";
+    private static final String user = "sql11417745";
+    private static final String password = "eIMih4pNNj";
+
+    private static Connection con;
+    private static Statement stmt;
+    private static ResultSet rs;
 
     public static void main(String[] args) throws IOException, TelegramApiException{
+        try {
+            // opening database connection to MySQL server
+            con = DriverManager.getConnection(url, user, password);
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        } finally {
+            //close connection ,stmt and resultset here
+            try { con.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
+        }
+
         Properties tgProps= new Properties();
         tgProps.load(App.class.getClassLoader().getResourceAsStream("tg.properties"));
         final Bot bot = new Bot("@simple_dimple_exam_bot","1814922061:AAG50huLS-2SZ7jtj6vqfo620Gr8hMj8aKk");
